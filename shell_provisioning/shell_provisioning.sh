93246cf06ed263e0ea0d3b54bb4349c62c81b521
mnh van dz <mnhvandz@gmail.com>
2015-01-27 05:02:48 +0100

    Few improvements

--- shell_provisioning/shell_provisioning.sh
+++ shell_provisioning/shell_provisioning.sh
@@ -1,62 +1,75 @@
 #!/usr/bin/env bash
 
 # update and upgrade on the guest virtual machine
-sudo apt-get update
-sudo apt-get upgrade -y
+echo "Updating ..."
+sudo apt-get update 
+echo "Upgrading ..."
+sudo apt-get upgrade -y 
+
 
 # Install apache 2
-sudo apt-get install apache2 -y
+echo "Installing apache2 ..."
+sudo apt-get install apache2 -y 
 
 # enable mod_rewrite
-sudo a2enmod rewrite
-
+echo "Enabling rewrite mode for apache2 ..."
+sudo a2enmod rewrite 
+ 
+echo "Enabling vhost_alias for apache2 ..."
 # enable vhost_alias
-sudo a2enmod vhost_alias
+sudo a2enmod vhost_alias 
 
-# enable vhost_alias
-sudo a2enmod status
-
-# TODO setup hosts file
-
-# Create symbolic link to the projects folder
-ln -fs /vagrant/projects /var/www
-
-ProjectName='webapp'
-WebRootFolder='web'
-
-# setup hosts file to 
-VHOST=$(cat <<EOF
-ServerName webserver.dev
-<VirtualHost *:80>
-    DocumentRoot "/var/www/${ProjectName}/${WebRootFolder}"
-    <Directory "/var/www/${ProjectName}/${WebRootFolder}">
-        AllowOverride All
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
-)
-echo "${VHOST}" > /etc/apache2/sites-available/"${ProjectName}".conf 
-sudo a2dissite 000-default.conf 2> /dev/null
-sudo a2ensite "${ProjectName}".conf 2> /dev/null
-sudo service apache2 reload 2> /dev/null
-
-#Install php5 & related modules
-sudo apt-get install php5 php5-cli php5-common libapache2-mod-php5 -y
-sudo apt-get install mcrypt php5-mcrypt php5-intl php5-adodb php5-gd php-pear -y
-sudo apt-get libgd-tools libmcrypt-dev php5-mysql php5-curl -y
-
-#enabling few modules
-sudo php5enmod mcrypt adodb gd json intl mcrypt mysqli mysql pdo_mysql
 
 # Password to use fro phpmyadmin and MySql
 UniquePassWord='upw'
 
+echo "Installing MySql ..."
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $UniquePassWord"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $UniquePassWord"
-sudo apt-get -y install mysql-server 
+sudo apt-get -y install mysql-server  
 
+
+# Note: Since the MySQL bind-address has a tab cahracter I comment out the end line
+sudo sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/my.cnf
+
+#mysql -u root -p "upw" -Bse "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'upw' WITH GRANT OPTION;"
+sudo service mysql restart
+
+
+echo "Install various software ..."
+sudo apt-get install libgd-tools -y 
+sudo apt-get install libmcrypt-dev -y 
+
+
+# Installing PHP & related modules
+echo "Install PHP & related modules ..."
+sudo apt-get install libapache2-mod-php5 -y 
+sudo apt-get install php5 -y 
+sudo apt-get install php5-cli -y 
+sudo apt-get install php5-common -y 
+sudo apt-get install php5-dev -y 
+sudo apt-get install php5-gd -y 
+sudo apt-get install php5-json -y 
+sudo apt-get install php5-pgsql -y 
+sudo apt-get install php5-sqlite -y 
+sudo apt-get install php5-memcached -y 
+sudo apt-get install php-pear -y 
+sudo apt-get install php5-adodb -y 
+sudo apt-get install php5-curl -y 
+sudo apt-get install php5-intl -y 
+sudo apt-get install php5-mcrypt -y 
+sudo apt-get install php5-tidy -y 
+sudo apt-get install php5-readline -y 
+sudo apt-get install php5-mysql -y 
+sudo apt-get install php5-xdebug -y 
+
+
+# Enabling modules
+echo "Enabling PHP modules ..."
+sudo php5enmod mcrypt adodb gd json intl mcrypt mysqli mysql pdo_mysql 
+
+echo "Installing PhpMyAdmin ..."
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
@@ -64,15 +77,32 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm pass
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $UniquePassWord"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $UniquePassWord"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
+sudo apt-get -y install phpmyadmin 
 
 # restart apache
-sudo service apache2 restart 
+echo "Disable default vhost..."
+sudo a2dissite 000-default.conf
 
+# restart apache
+echo "Restart Apache2..."
+sudo service apache2 restart  
 
 # install git
-sudo apt-get -y install git
+echo "Installing Git ..."
+sudo apt-get -y install git 
 
 # install Composer
+echo "Installing Composer ..."
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+
+#Install composer-plugins
+echo "installing composer-asset-plugin needed to install Yii2"
+composer global require "fxp/composer-asset-plugin:1.0.0-beta4"
+
+echo "Executing autoremove ..."
+sudo apt-get autoremove -y 
+
+sudo updatedb
+
+
